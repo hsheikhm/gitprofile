7c1eb280d8c8e1537d7f5fefadbe427e750f58dd
Hamza Sheikh <hsheikhm@gmail.com>
2015-12-03 10:46:40 +0000

    passed feature test: input name

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -1,7 +1,17 @@
 describe('Github Profile finder', function(){
+
   it('has a title', function(){
     browser.get('http://localhost:8080');
 
     expect(browser.getTitle()).toEqual('Github User Search');
+  });
+
+  it('finds profiles', function() {
+    browser.get('http://localhost:8080');
+
+    element(by.model('searchCtrl.searchTerm')).sendKeys('spike01');
+    element(by.className('btn')).click();
+
+    expect(element(by.binding('user.login')).getText()).toEqual('spike01');
   });
 });
