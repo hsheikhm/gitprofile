173030693b9715dd640c6fda516eabe2acf31d77
Hamza Sheikh <hsheikhm@gmail.com>
2015-12-03 16:08:09 +0000

    completed styling

--- js/searchFactory.js
+++ js/searchFactory.js
@@ -7,7 +7,8 @@ githubUserSearch.factory('Search', ['$http', function($http){
         url: queryUrl,
         method: 'GET',
         params: {
-          'q': searchTerm
+          'q': searchTerm,
+          'access_token': 'ff0e33ae3b97a0f52096f0142ad0beba6f0a3e0f'
         }
       });
     }
