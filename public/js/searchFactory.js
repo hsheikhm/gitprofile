98e1a2c52867919da6779fec02ad1991a87fb146
ALRW <awerner1@googlemail.com>
2015-12-03 16:20:22 +0000

    Created Sinatra Backend

--- public/js/searchFactory.js
+++ public/js/searchFactory.js
@@ -7,8 +7,7 @@ githubUserSearch.factory('Search', ['$http', function($http){
         url: queryUrl,
         method: 'GET',
         params: {
-          'q': searchTerm,
-          'access_token': 'ff0e33ae3b97a0f52096f0142ad0beba6f0a3e0f'
+          'q': searchTerm
         }
       });
     }
