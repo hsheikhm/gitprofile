5df5207637ca42d2e4cb3a8c277781ddfe683d9a
Hamza Sheikh <hsheikhm@gmail.com>
2015-12-04 10:22:56 +0000

    removed access token

--- public/js/searchFactory.js
+++ public/js/searchFactory.js
@@ -7,27 +7,20 @@ githubUserSearch.factory('Search', ['$http', function($http){
         url: queryUrl,
         method: 'GET',
         params: {
-          'q': searchTerm,
-          'access_token': '7e2741cf0e27e9104ce366c2b251d393bc93a1c5'
+          'q': searchTerm
         }
       });
     },
     followersQuery: function(username) {
       return $http({
         url: 'https://api.github.com/users/' + username + '/followers',
-        method: 'GET',
-        params: {
-          'access_token': '7e2741cf0e27e9104ce366c2b251d393bc93a1c5'
-        }
+        method: 'GET'
       });
     },
     reposQuery: function(username) {
       return $http({
         url: 'https://api.github.com/users/' + username + '/repos',
-        method: 'GET',
-        params: {
-          'access_token': '7e2741cf0e27e9104ce366c2b251d393bc93a1c5'
-        }
+        method: 'GET'
       });
     },
   };
